bcab6ea5aa405ef990ef732781a8e43c70952a47
allyson7 <allysonlopes0@gmail.com>
2022-09-21 16:42:45 -0400

    :construction: working on feature to add products in cart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,10 +23,11 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
   //   // TODO
+  //   if ()
   // }, {} as CartItemsAmount)
 
   useEffect(() => {
@@ -34,13 +35,13 @@ const Home = (): JSX.Element => {
       // TODO
       api.get("/products").then((response) => setProducts(response.data));
     }
-    console.log(products);
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
     // TODO
+    addProduct(id);
   }
 
   return (
@@ -53,7 +54,7 @@ const Home = (): JSX.Element => {
           <button
             type="button"
             data-testid="add-product-button"
-            // onClick={() => handleAddProduct(product.id)}
+            onClick={() => handleAddProduct(product.id)}
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
